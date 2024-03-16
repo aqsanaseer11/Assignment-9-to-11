91a9083dfc07d663606e075d732b0670d34a4d62
Aqsa Naseer <arzoosultani123456@gmail.com>
2024-03-17 00:12:03 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,27 @@
+// Task:9;Favorite Number: Store your favorite number in a variable. 
+//Then, create a message revealing your favorite number. Print that message.
+
+
+let favouriteNumber: number = 10;
+console.log(`My fovourite number is ${favouriteNumber}.`)
+
+// Task:10;Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+//If the programs are straightforward at this point, just add your name and the current date at the top of each program file.
+//Then, write one sentence describing what the program does.
+
+
+//Asking for learning..
+let firstname:string="Aqsa Naseer";
+console.log(`Hello ${firstname}, would you like to learn some Python today?`);
+
+// Task:11 Names: Store the names of a few of your friends in an array called names. 
+//Print each person’s name by accessing each element in the list, one at a time.
+
+ let names:string[]=[
+     "Asia","Anum","Alia"
+ ];
+ for(let i=0;i<names.length;i++){
+     console.log(names[i]);
+ }
+
+
